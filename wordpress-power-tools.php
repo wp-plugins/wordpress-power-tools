eda339fdf004e3c0b27683c4185fed39a75fc630
matstars <matstars@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-14 16:23:18 +0000

    This plugin is no longer maintained.
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-power-tools/trunk@804446 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-power-tools.php
+++ wordpress-power-tools.php
@@ -2,13 +2,10 @@
 /*
 Plugin Name: WordPress Power Tools
 Plugin URI: http://matgargano.com/wordpress-power-tools/
-Description: A suite of tools that takes care of non-sense dirty work on WordPress installs.
-Version: 1.4.3
+Description: This plugin is no longer maintained.
+Version: 2.0
 Author: Mat Gargano
 Author URI: http://matgargano.com
-Graphics retrieved from iconfinder.com ; 
-  WordPress PowerTools Admin Menu graphic (Yusuke Kamiyamane, http://p.yusukekamiyamane.com/)
-  WordPress PowerTools Admin Page graphic (Everaldo Coelho, http://www.everaldo.com/)  
 
 
 License: Copyright 2011  Matthew Gargano  (email : mgargano@gmail.com)
@@ -28,67 +25,5 @@ License: Copyright 2011  Matthew Gargano  (email : mgargano@gmail.com)
     
 */
 
-register_activation_hook(__FILE__, 'WPPT_activate_plugin');
-register_deactivation_hook(__FILE__, 'WPPT_deactivate_plugin');
-define('WPPT_MIN_WP_VERSION','3.2');
-define('WPPT_MIN_PHP_VERSION','5.2');
-$plugin_prefix="WPPT";
-function WPPT_deactivate_plugin() {
-  global $plugin_prefix;
-  delete_option($plugin_prefix."_active");
-  delete_option($plugin_prefix."_options");
-}
-  
-function WPPT_activate_plugin() {
-  global $plugin_prefix;
 
-  $wp_version = get_bloginfo('version');
-  if ( version_compare( phpversion(), WPPT_MIN_PHP_VERSION, '<' ) ) {
-    trigger_error('', E_USER_ERROR);
-  }
-  if ( version_compare( $wp_version, WPPT_MIN_WP_VERSION, '<' ) ){
-    trigger_error('', E_USER_ERROR);
-  }
-}
- 
-function _WPPT_activation_message() {
-  global $plugin_prefix;
-  global $activation_notice;
-  $wp_version = get_bloginfo('version');
-  
-  if ( get_option ( $plugin_prefix."_active" ) <> 1 ) {
-    if ( trim ( $activation_notice ) != "" ) {
-      echo $activation_notice;
-    }
-    update_option($plugin_prefix."_active",1);
-  }
-}
-    
-if ( $_GET['action'] == 'error_scrape' ) {
-  if ( version_compare ( phpversion(), WPPT_MIN_PHP_VERSION, '<' ) ) {
-    $errors[] = "PHP Version " . WPPT_MIN_PHP_VERSION." or higher, you are using ".phpversion();
-  } 
-  if ( version_compare ( get_bloginfo('version'), WPPT_MIN_WP_VERSION, '<' ) ){
-    $errors[] = "WordPress ".WPPT_MIN_WP_VERSION." or higher, you are using ".get_bloginfo('version');
-  }
-  $counter = 0;
-  if ( count ( $errors ) > 0 ) {
-    foreach ( $errors as $error ) {
-      if( $counter > 0 ) {
-        $error_output .= " and ";
-      }
-      $error_output .= $error;
-      $counter++;
-    }
-    $error_output = "This plugin requires ".$error_output.". Contact your hosting provider or system administrator and ask them to upload to the latest versions.";
-    die($error_output);
-  }
- } else {
-  add_action('admin_notices', '_WPPT_activation_message');
-  require_once("class-wp_power_tools.php");
-  require_once("class-wp_power_tools_admin.php");
-  $WP_power_tools_admin=new WP_Power_Tools_Admin;
-}
-
-  
 ?>
